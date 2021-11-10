148d57686b908870a42ca70e52a6ebb8f0d6cc91
Jaehun Park <pjaehun4122@gmail.com>
2021-11-09 19:57:00 -0800

    wip:

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,25 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt= require('jsonwebtoken')
+const Users = require('../users/users-model')
+
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization
+  if (!token) {
+    return next({ status: 401, message: 'Token required'})
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next({
+        status: 401,
+        message: "Token invalid"
+      })
+    }
+    req.decodedJwt = decoded
+    console.log(decoded)
+    next()
+  })
+ 
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -29,10 +48,26 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
+    if (req.decodedJwt.role !== role_name) {
+      next({
+        status: 403,
+        message: "This is not for you"
+      })
+    } else {
+      next()
+    }
 }
 
 
 const checkUsernameExists = (req, res, next) => {
+  let {username}=req.body
+  Users.findBy({username})
+  .then(([user])=>{
+    if(!user){
+      res.status(401).json({message:"Invalid credentials" })
+    } else { next()}
+  })
+  .catch(next)
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -44,6 +79,8 @@ const checkUsernameExists = (req, res, next) => {
 
 
 const validateRoleName = (req, res, next) => {
+  const {role_name}=req.body
+  
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
--- api/secrets/index.js
+++ api/secrets/index.js
@@ -7,5 +7,8 @@
   developers cloning this repo won't be able to run the project as is.
  */
 module.exports = {
-
+  PORT: process.env.PORT || 9000,
+  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 4,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  JWT_SECRET: process.env.JWT_SECRET || 'shh',
 }
